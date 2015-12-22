21018e02f1fe4347457de7d4378a7652c40e9ad2
JokieW <gulliverjko@gmail.com>
2015-12-22 15:07:37 -0500

    [GP] Spawning pebbles

---
+++ Assets/src/ObjectSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour 
+{
+
+    public GameObject thing;
+
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            GameObject.Instantiate(thing, transform.position + RandomVector(2.0f), Quaternion.identity);
+        }
+	}
+
+    public Vector3 RandomVector(float range)
+    {
+        return new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
+    }
+}
---
+++ Assets/src/StickyObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class StickyObject : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<StickyObject>() != null)
+        {
+            FixedJoint fj = gameObject.AddComponent<FixedJoint>();
+            fj.connectedBody = collision.gameObject.GetComponent<Rigidbody>();
+            fj.enableCollision = false;
+        }
+    }
+
+}
