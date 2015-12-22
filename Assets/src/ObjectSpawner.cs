58850312bb56a6aea8cbcfb856d50967956b5579
JokieW <gulliverjko@gmail.com>
2015-12-22 15:30:22 -0500

    [GP] Implemented trigger to spawn pebbles

--- Assets/src/ObjectSpawner.cs
+++ Assets/src/ObjectSpawner.cs
@@ -5,9 +5,16 @@ public class ObjectSpawner : MonoBehaviour
 {
 
     public GameObject thing;
+    public SteamVR_TrackedObject rightHand;
+    public SteamVR_TrackedObject leftHand;
+
 
 	void Update () 
     {
+        if (SteamVR_Controller.Input((int)rightHand.index).GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject.Instantiate(thing, transform.position + RandomVector(2.0f), Quaternion.identity);
+        }
         if (Input.GetKeyDown(KeyCode.K))
         {
             GameObject.Instantiate(thing, transform.position + RandomVector(2.0f), Quaternion.identity);
